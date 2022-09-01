d437c1c0e5de7bfc0e156f73955764363d2b51ca
Josue Torres <josuetolvera@gmail.com>
2022-09-01 13:50:54 -0500

    Add a grid container of  16x16 square divs

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 container = document.querySelector('.container');
 
-function createSketch () {
-  for (i = 0; i < 16; i++) {
+function createRow() {
+  for (i = 0; i < 256; i++) {
     const squareDiv = document.createElement('div');
-    squareDiv.classList = 'square';
-    squareDiv.textContent = 'Hola mundo!';
+    squareDiv.classList = 'square-div';
     container.appendChild(squareDiv);
   }
 }
 
-createSketch();
+createRow();
+
