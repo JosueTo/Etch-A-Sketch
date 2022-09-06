ab03d1f93374e2f3535eebdaee5db2e2f2dbe75e
Josue Torres <josuetolvera@gmail.com>
2022-09-05 20:47:35 -0500

    Restart the project from scratch

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
-const container = document.querySelector('.container');
 
-// Create the grid divs
-function createDivs(size) {
-  container.style.setProperty('--size', size)
-  for (i = 0; i < size * size; i++) {
-    const squareDiv = document.createElement('div');
-    squareDiv.classList = 'square-div';
-    container.appendChild(squareDiv);
-  }
-}
-
-createDivs(8);
 
