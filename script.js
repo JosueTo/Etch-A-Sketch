4eb3633dd225eda4feaaabacf5f2bef5e5078647
Josue Torres <josuetolvera@gmail.com>
2022-09-07 14:41:06 -0500

    Add a function to create the default board

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
+function createBoard(size) {
+  let board = document.querySelector('.container');
 
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  
+  let numDivs = size * size;
 
+  for (let i = 0; i < numDivs; i++) {
+    let div = document.createElement('div');
+    div.style.border = '1px black solid';
+    board.appendChild(div).classList.add('box');
+  }
+
+}
+
+createBoard(3);
