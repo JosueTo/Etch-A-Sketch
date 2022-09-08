91985c7037eec56bb1ed00023811414a0c20d8b9
Josue Torres <josuetolvera@gmail.com>
2022-09-07 22:04:33 -0500

    Add a function to change the size of the board

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-function createBoard(size) {
-  let board = document.querySelector('.container');
+const board = document.querySelector('.container');
+const buttonSize = document.querySelector('#size');
 
-  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-  
+function createBoard(size) {
+    
   let numDivs = size * size;
 
   for (let i = 0; i < numDivs; i++) {
     let div = document.createElement('div');
     div.style.border = '1px black solid';
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     board.appendChild(div).classList.add('box');
   }
 
 }
 
-createBoard(3);
+function getSize() {
+  let input = prompt('What size your board would you like to be?');
+
+  let message =  document.querySelector('#message');
+  if (input === '' || input == isNaN) {
+    message.textContent = 'Please provide a number';
+  } else if (input < 16 || input > 100) {
+    message.textContent = 'Please provide a number between 16 and 100';
+  } else {
+    message.textContent = 'Have fun!'
+    return input;
+  }
+}
+
+
+buttonSize.addEventListener('click', () => {
+  removeDivs();
+  let size = getSize();
+  createBoard(size);
+})
+
+function removeDivs() {
+  const boxs = board.querySelectorAll('.box');
+  boxs.forEach(box => box.remove()); 
+}
+
+createBoard(16);
